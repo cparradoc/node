dd233a00afd4f3263d6626da5a7ef8aabdd175cf
cparradoc <cparradoc@minsait.com>
2022-03-08 11:38:59 +0100

    mongoose

--- node_express/index.js
+++ node_express/index.js
@@ -1,20 +1,10 @@
 const express = require('express');
 
+// Requerimos el archivo de configuración de nuestra DB
+require('./db.js');
+
 const PORT = 3000;
 const server = express();
-
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send('Hello Minsait!');
-});
-
-router.get('/movies/:name', (req, res) => {
-    const name = req.params.name;
-    console.log(name);
-});
-
-server.use('/', router);
 
 server.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
