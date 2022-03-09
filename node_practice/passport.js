779455f8bcf38018cfe5243a11c0ba90523b5026
cparradoc <cparradoc@minsait.com>
2022-03-09 13:12:10 +0100

    passport register new users

---
+++ node_practice/passport.js
@@ -0,0 +1,50 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+const User = require('./models/User');
+
+// Creamos los salts de bcrypt
+const saltRounds = 10;
+
+passport.use(
+  'register', // Nombre de la estrategia, en este caso será register
+  new LocalStrategy(
+    {
+      usernameField: 'email', // Elegimos el campo email del req.body
+      passwordField: 'password', // Elegimos el campo password del req.body
+      passReqToCallback: true, // Hace que el callback reciba la Request (req)
+    },
+    async (req, name, type, password, mail, done) => {
+      try {
+        // Primero buscamos si el usuario existe en nuestra DB
+        const previousUser = await User.findOne({ mail: mail });
+
+        // Si hay usuario previamente, lanzamos un error
+        if (previousUser) {
+          const error = new Error('The user is already registered!');
+          return done(error);
+        }
+
+        // Si no existe el usuario, vamos a "hashear" el password antes de registrarlo
+        const hash = await bcrypt.hash(password, saltRounds);
+
+        // Creamos el nuevo user y lo guardamos en la DB
+        const newUser = new User({
+            name: name,
+            type: type,
+            password: hash,
+            mail: mail,
+        });
+
+        const savedUser = await newUser.save();
+        
+        // Invocamos el callback con null donde iría el error y el usuario creado
+        done(null, savedUser);
+      } catch (err) {
+        // Si hay un error, resolvemos el callback con el error
+        return done(err);
+      }
+    }
+  )
+);
