b3458761ad4017fc51d12107624b138a15ea676a
cparradoc <cparradoc@minsait.com>
2022-03-09 11:25:22 +0100

    app use mongodb

--- node_practice/app.js
+++ node_practice/app.js
@@ -8,3 +8,15 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening in http://localhost:${PORT}`);
 });
+
+app.use(
+    session({
+      secret: 'game_store', // ¡Este secreto tendremos que cambiarlo en producción!
+      resave: false, // Solo guardará la sesión si hay cambios en ella.
+      saveUninitialized: false, // Lo usaremos como false debido a que gestionamos nuestra sesión con Passport
+      cookie: {
+        maxAge: 3600000, // Milisegundos de duración de nuestra cookie, en este caso será una hora.
+      },
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  );
