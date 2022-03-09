3d59e6a6ba3b8718e1c929b1e1fc322307b80913
cparradoc <cparradoc@minsait.com>
2022-03-09 12:34:55 +0100

    user model

---
+++ node_practice/db/models/User.js
@@ -0,0 +1,21 @@
+// Archivo User.js dentro de la carpeta models
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Creamos el esquema de usuarios
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    type: {type: String, required: true},
+    password: {type: String, required: true},
+    mail: {type: String, required: true},
+  {
+    // Esta propiedad servirá para guardar las fechas de creación y actualización de los documentos
+    timestamps: true,
+  }
+);
+
+// Creamos y exportamos el modelo User
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
