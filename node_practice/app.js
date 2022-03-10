0f75fa3cb490757f9ca16c38ef2a09033c4c7f39
cparradoc <cparradoc@minsait.com>
2022-03-10 11:09:01 +0100

    mongostore connect fix

--- node_practice/app.js
+++ node_practice/app.js
@@ -56,7 +56,7 @@ passport.deserializeUser(async (userId, done) => {
 app.use('/users', userRouter);
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongodb-session')(session);
 
 app.use(
     session({
