ec64246b216deb09c070a327f471c468d660b509
cparradoc <cparradoc@minsait.com>
2022-03-11 10:55:40 +0100

    redorder app

--- node_practice/app.js
+++ node_practice/app.js
@@ -12,19 +12,34 @@ const passport = require('passport');
 require('./passport'); // Requerimos nuestro archivo de configuración
 
 
+const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  session({
+    secret: 'game_store', // ¡Este secreto tendremos que cambiarlo en producción!
+    resave: false, // Solo guardará la sesión si hay cambios en ella.
+    saveUninitialized: false, // Lo usaremos como false debido a que gestionamos nuestra sesión con Passport
+    cookie: {
+      maxAge: 3600000, // Milisegundos de duración de nuestra cookie, en este caso será una hora.
+    },
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 const appRoutes = require('./routes/app.routes');
 
 app.use('/', appRoutes);
 const userRouter = require('./routes/user.routes');
 const productRouter = require('./routes/product.routes');
-
-const router = express.Router();
-
-const path = require('path');
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 
 
 // Esta función usará el usuario de req.LogIn para registrar su id en la cookie de sesión
@@ -47,22 +62,8 @@ const authMiddleware = require('./middlewares/auth.middleware');
 app.use('/users', [authMiddleware.isAuthenticated], userRouter);
 app.use('/products', [authMiddleware.isAuthenticated], productRouter);
 const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongodb-session')(session);
 
-app.use(
-    session({
-      secret: 'game_store', // ¡Este secreto tendremos que cambiarlo en producción!
-      resave: false, // Solo guardará la sesión si hay cambios en ella.
-      saveUninitialized: false, // Lo usaremos como false debido a que gestionamos nuestra sesión con Passport
-      cookie: {
-        maxAge: 3600000, // Milisegundos de duración de nuestra cookie, en este caso será una hora.
-      },
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    })
-  );
-app.use(passport.initialize());
-app.use(passport.session());
+
 
 app.use('*', (req, res, next) => {
   const error = new Error('Route not found'); 
