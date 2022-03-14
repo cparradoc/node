440d41acf274fe9dda3283c0649cdf8068147760
cparradoc <cparradoc@minsait.com>
2022-03-14 15:59:46 +0100

    encapsulate in a function to hash password

--- node_practice/db/seeds/userSeed.js
+++ node_practice/db/seeds/userSeed.js
@@ -1,39 +1,44 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = require('./models/User');
 
-const users = [
-    {
-        name: "admin",
-        type: "admin",
-        password: "uFUcfG2agO9CJHxI",
-        mail: "cparradoc@mail.com"
-    }
-];
+function hashPassword() {
+  const users = [
+      {
+          name: "admin",
+          type: "admin",
+          password: "uFUcfG2agO9CJHxI",
+          mail: "cparradoc@mail.com"
+      }
+  ];
 
-const userDocuments = users.map(users => new Product(users));
+  const userDocuments = users.map(users => new Product(users));
 
-const DB_URL = "mongodb+srv://admin:uFUcfG2agO9CJHxI@cluster0.h9fct.mongodb.net/users?retryWrites=true&w=majority";
+  const DB_URL = "mongodb+srv://admin:uFUcfG2agO9CJHxI@cluster0.h9fct.mongodb.net/users?retryWrites=true&w=majority";
 
-mongoose
-  .connect(DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(async () => {
-    // Utilizando Pet.find() obtendremos un array con todos los pets de la db
-    const allUsers = await User.find();
-    
-    // Si existen pets previamente, dropearemos la colección
-    if (allUsers.length) {
-      await User.collection.drop();
-    }
-  })
-  .catch((err) => console.log(`Error deleting data: ${err}`))
-  .then(async () => {
-    // para llenar nuestra base de datos con todas las mascotas.
-    await User.insertMany(userDocuments);
-  })
-  .catch((err) => console.log(`Error creating data: ${err}`))
-  // Por último nos desconectaremos de la DB.
-  .finally(() => mongoose.disconnect());
+  mongoose
+    .connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(async () => {
+      // Utilizando Pet.find() obtendremos un array con todos los pets de la db
+      const allUsers = await User.find();
+      
+      // Si existen pets previamente, dropearemos la colección
+      if (allUsers.length) {
+        await User.collection.drop();
+      }
+    })
+    .catch((err) => console.log(`Error deleting data: ${err}`))
+    .then(async () => {
+      // para llenar nuestra base de datos con todas las mascotas.
+      await User.insertMany(userDocuments);
+    })
+    .catch((err) => console.log(`Error creating data: ${err}`))
+    // Por último nos desconectaremos de la DB.
+    .finally(() => mongoose.disconnect());
+  }
+
+  hashPassword();
