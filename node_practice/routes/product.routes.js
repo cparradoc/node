e6203f16f63c1ac68d10cef54b1968a5dbf56bbc
cparradoc <cparradoc@minsait.com>
2022-03-15 11:34:07 +0100

    routing to single game products

--- node_practice/routes/product.routes.js
+++ node_practice/routes/product.routes.js
@@ -11,6 +11,18 @@ router.get('/', async (req, res) => {
     } catch (err) {
       next(err);
     }
-  });
+});
+
+router.get('/product/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const product = await Product.findById(id);
+    if(product) {
+      return res.status(200).render('product', {title: "Game Store", id: id, product: product});
+    }
+  } catch (err) {
+    return res.status(500).render('product', {title: "Game Store"});
+  }
+});
 
   module.exports = router;
